3b8f81969f3bacc64b90aec1b9a4815a4631dbc2
final <619479768@qq.com>
2020-04-15 19:11:05 +0800

    编写协程库

--- zh/coroutine.cpp
+++ zh/coroutine.cpp
@@ -3,18 +3,47 @@
 namespace zh
 {
 
-Coroutine::Coroutine(callback pfn, uint stackSize) :
-    m_pfn(pfn)
+static __thread ThrCoroutine* thrCoroutine;
+
+Coroutine::Coroutine() :
+    m_pfn(nullptr),
+    m_args(nullptr),
+    m_stackSize(0),
+    m_stack(nullptr)
+{
+}
+
+Coroutine::Coroutine(callback pfn, void* args, int stackSize = CO_STACK_SIZE) :
+    m_pfn(pfn),
+    m_args(args)
 {
     m_stackSize = stackSize ? stackSize : CO_STACK_SIZE;
-    m_stack = new char[stackSize];
+    m_stack = malloc(m_stackSize);
 
     getcontext(&m_context);
     m_context.uc_link = nullptr;
     m_context.uc_stack.ss_sp = m_stack;
     m_context.uc_stack.ss_size = m_stackSize;
+}
+
+void Coroutine::resume()
+{
 
 }
 
+ThrCoroutine* Coroutine::getCurrThr()
+{
+    if (thrCoroutine)
+        return thrCoroutine;
+    
+    thrCoroutine = new ThrCoroutine();
+    return thrCoroutine;
+}
+
+ThrCoroutine::ThrCoroutine()
+{
+    Coroutine* coroutine = new Coroutine();
+    pCoStack.push(coroutine);
+}
 
 }
--- zh/coroutine.h
+++ zh/coroutine.h
@@ -2,21 +2,22 @@
 #define __ZH_COROUTINE_H__
 
 #include <ucontext.h>
+#include <stack>
+#include <memory>
+#include <stdlib.h>
 
 namespace zh
 {
 #define CO_STACK_SIZE 128 * 1024
-#define uint unsigned int
 
-static __thread Coroutine* t_coroutine;
+typedef void (*callback)(void*);
 
-typedef void (*callback)();
-
+class ThrCoroutine;
 class Coroutine
 {
 public:
 
-    Coroutine(callback pfn, uint stackSize = CO_STACK_SIZE);
+    Coroutine(callback pfn, void* args, int stackSize = CO_STACK_SIZE);
     ~Coroutine();
     void resume();
     void yield();
@@ -27,18 +28,28 @@ private:
     Coroutine();
 
 public:
-    static Coroutine* getThis();
-    static void mainFun();
-    static void setThis(Coroutine* Coroutine);
-    static void yield();
+    static void yieldCurrThr();
+    static void coroutineFunc(Coroutine* coroutine);
+    static ThrCoroutine* getCurrThr();
 
 private:
+    friend ThrCoroutine;
     callback m_pfn;
-    uint m_stackSize;
+    void* m_args;
+
+    int m_stackSize;
     void* m_stack;
+
     ucontext_t m_context;
 };
 
+class ThrCoroutine
+{
+private:
+    ThrCoroutine();
+    friend Coroutine;
+    std::stack<Coroutine*> pCoStack;
+};
     
 }
 
