8acc856880013f3ff4e3543a7596634176931e8e
final <619479768@qq.com>
2021-09-15 21:21:02 +0800

    无锁队列

--- freelockqueue.h
+++ freelockqueue.h
@@ -2,100 +2,141 @@
 
 #include <atomic>
 
-template <typename T>
+template <typename T, unsigned int N = 32>
 class freelockqueue
 {
-    enum class retype
-    {
-        enqueue_chunk_complate,
-        enqueue_chunk_new,
-        dequeue_chunk_free,
-        dequeue_chunk_empty,
-        dequeue_chunk_complate
-    };
-
     typedef unsigned int index_t;
+    using value_t= char[sizeof(T)];
+
+    enum result_status
+    {
+        enqueue_complate,
+        enqueue_chunk_not_enough,
+
+        dequeue_complate,
+        dequeue_chunk_end,
+        dequeue_chunk_empty
+    };
 
     struct chunk
     {
-        retype enqueue(T&& value)
+        template<typename U>
+        result_status enqueue(U&& value)
         {
-            index_t tail_index = _tail_index.load();
+            index_t tail_index;
             do
             {
-                if (tail_index >= 32)
-                    return enqueue_chunk_new;
-            } while (!tail_index.compare_exchange_weak(p, p + 1));
-            
-            _value[tail_index] = std::move(value);
+                tail_index = _tail_index.load(std::memory_order_relaxed);
 
-            return enqueue_chunk_complate;
+                if (tail_index >= N)
+                    return enqueue_chunk_not_enough;
+
+            } while (!_tail_index.compare_exchange_weak(tail_index, tail_index + 1, std::memory_order_relaxed));
+            
+            new (_value[tail_index]) T(std::move(value));
+            return enqueue_complate;
         }
 
-        retype dequeue(T& value)
+        result_status dequeue(T& value)
         {
             index_t head_index;
             index_t tail_index;
 
-            head_index = _head_index.load();
-            tail_index = _tail_index.load();
+            do
+            {
+                head_index = _head_index.load(std::memory_order_relaxed);
+                tail_index = _tail_index.load(std::memory_order_relaxed);
 
-            if (tail_index >= 32)
-                return dequeue_chunk_free;
+                if (head_index >= N)
+                    return dequeue_chunk_end;
 
-            if (head_index >= tail_index)
-                return dequeue_chunk_empty;
+                if (head_index >= tail_index)
+                    return dequeue_chunk_empty;
+
+            } while (!_head_index.compare_exchange_weak(head_index, head_index + 1, std::memory_order_relaxed));
             
-            value = std::move(_value[head_index]);
-            return dequeue_chunk_complate;
+            value = std::move((T)_value[head_index]);
+            return dequeue_complate;
         }
 
-        T _value[32];
+        value_t _value[N];
         std::atomic<index_t> _head_index{ 0 };
         std::atomic<index_t> _tail_index{ 0 };
         chunk* _next{ nullptr };
     };
 
-
 public:
     freelockqueue(){}
     ~freelockqueue(){}
 
-    void enqueue(T&& vaule)
+    template<typename U>
+    void enqueue(U&& vaule)
     {
-        while (_head_chunk->enqueue(std::forward(vaule) == enqueue_chunk_new)
+        tail_chunk = _tail_chunk.load(std::memory_order_relaxed);
+        result_status rs = tail_chunk->enqueue(std::forward<U>(vaule);
+        while ( res == enqueue_chunk_not_enough)
         {
             bool new_chunk = false;
             if (_new_chunk.compare_exchange_strong(new_chunk, true))
             {
-                if (_head_chunk->enqueue(std::forward(vaule)) == enqueue_chunk_new)
+                rs = _tail_chunk.load(std::memory_order_relaxed)->enqueue(std::move(vaule));
+                if (rs == enqueue_chunk_not_enough)
                 {
-                    new_chunk();
-                    _head_chunk->enqueue(std::forward(vaule));
-                    _new_chunk.exchange(false);
-                    return;
+              /*      std::atomic<chunk_t*> ck;
+                    ck.store(new chunk_t);*/
+                    chunk* ck = new chunk;
+                    _tail_chunk.load(std::memory_order_relaxed)->_next = ck;
+                    _tail_chunk.store(ck);
+
+                    _new_chunk.store(false, std::memory_order_release);
+
+                    rs = _tail_chunk.load(std::memory_order_relaxed)->enqueue(std::move(vaule));
+                    if (rs != enqueue_complate)
+                        continue;
+                    else
+                        break;
                 }
+                _new_chunk.store(false, std::memory_order_release);
             }
         }
     }
 
-    void dequeue(T& vaule)
+    bool dequeue(T& value)
     {
+        result_status rs = _head_chunk.load(std::memory_order_relaxed)->dequeue(value);
+        //while ((res = _head_chunk->dequeue(value)) == dequeue_chunk_end)
+        //{
 
+        //}
+
+        if (rs == dequeue_chunk_end)
+        {
+            if (_head_chunk.load(std::memory_order_relaxed)->_next != nullptr)
+            {
+                chunk* ck = _head_chunk.load(std::memory_order_relaxed);
+                _head_chunk.store( _head_chunk.load(std::memory_order_relaxed)->_next);
+                delete ck;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (rs == dequeue_chunk_empty)
+        {
+            return false;
+        }
+        else if (rs == dequeue_complate)
+        {
+            return true;
+        }
+        return false;
     }
 
 private:
-    void new_chunk()
-    {
-        chunk* ck = new chunk;
-        _tail_chunk->_next = ck;
-        _tail_chunk = ck;
-    }
-
-private:
-    chunk* _head_chunk{ new chunk };
-    chunk* _tail_chunk{ &chunk };
+    std::atomic<chunk*> _head_chunk{ new chunk };
+    std::atomic<chunk*> _tail_chunk{ _head_chunk.load(std::memory_order_relaxed) };
 
     std::atomic<int> _size{ 0 };
-    std::atomic<int> _new_chunk { false };
+    std::atomic<bool> _new_chunk { false };
 };
